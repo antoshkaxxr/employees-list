75e1b61793fca513c9eb06db497975bf2a5a328d
antoshkaxxr <gbanton2004@yandex.ru>
2025-01-24 12:21:13 +0500

    some fixes

--- src/components/PagesPath/PagesPath.tsx
+++ src/components/PagesPath/PagesPath.tsx
@@ -2,16 +2,23 @@ import './PagesPath.css';
 import { ChevronRight } from "akar-icons";
 import {Link} from "react-router-dom";
 import {AppRoute} from "../../const";
+import {useWindowWidth} from "../../hooks/useWindowWidth";
 
 type PagesPathProps = {
     employeeName?: string | undefined;
 }
 
 export function PagesPath({ employeeName = undefined }: PagesPathProps) {
+    const windowWidth = useWindowWidth();
+
     return (
         <div className={'path-container'}>
-            <span className={'path-part'}>Главная</span>
-            <span><ChevronRight className={'chevron-right'} /></span>
+            {(windowWidth > 768 || location.pathname === AppRoute.Main) &&
+                <>
+                    <span className={'path-part'}>Главная</span>
+                    <span><ChevronRight className={'chevron-right'} /></span>
+                </>
+            }
             <Link to={AppRoute.Main} style={{ textDecoration: 'none' }}>
                 <span className={'path-part'}>Список сотрудников</span>
             </Link>
